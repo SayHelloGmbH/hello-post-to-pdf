907fb7bba342eba8838bdf152444c9f9898aa07e
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-03-19 15:27:46 +0100

    Adds language suffix to filename  if WPML is active

--- Classes/FileSystem.php
+++ Classes/FileSystem.php
@@ -1,50 +1,50 @@
 <?php
 namespace SayHello\PostToPDF;
 
-class FileSystem
-{
+class FileSystem {
+
 
 	private $folders;
 
-	public function getBaseDir()
-	{
+	public function getBaseDir() {
 		$this->folders = wp_upload_dir();
 		return $this->folders['basedir'];
 	}
 
-	public function getFilepath($post_id)
-	{
+	public function getFilepath( $post_id ) {
 
-		$folder = implode(DIRECTORY_SEPARATOR, [
-			$this->getBaseDir(),
-			get_post_type($post_id)
-		]);
+		$folder = implode(
+			DIRECTORY_SEPARATOR,
+			[
+				$this->getBaseDir(),
+				get_post_type( $post_id ),
+			]
+		);
 
-		$this->ensureFolderExists($folder);
+		$this->ensureFolderExists( $folder );
 
-		$filepath = implode(DIRECTORY_SEPARATOR, [
-			$folder,
-			get_post_field('post_name', get_post($post_id)).'.pdf'
-		]);
+		$language_suffix = '';
+
+		if ( function_exists( 'wpml_get_language_information' ) ) {
+			$language_information = wpml_get_language_information( $post_id );
+			$language_suffix      = isset( $language_information['language_code'] ) && ! empty( $language_information['language_code'] ) ? '-' . $language_information['language_code'] : '';
+		}
+
+		$filepath = implode(
+			DIRECTORY_SEPARATOR,
+			[
+				$folder,
+				get_post_field( 'post_name', get_post( $post_id ) ) . $language_suffix . '.pdf',
+			]
+		);
 
 		return $filepath;
 	}
 
-	public function ensureFolderExists($folder)
-	{
-		if (!is_dir($folder)) {
-			@mkdir($folder, 0755, true);
+	public function ensureFolderExists( $folder ) {
+		if ( ! is_dir( $folder ) ) {
+			@mkdir( $folder, 0755, true );
 		}
 	}
 
-	public function downloadFile($filepath, $filename)
-	{
-		header('Content-Type: '.mime_content_type($filepath));
-		header('Pragma: public');
-		header('Expires: -1');
-		header('Cache-Control: public, must-revalidate, post-check=0, pre-check=0');
-		header('Content-Disposition: attachment; filename="'.$filename.'"');
-		readfile($filepath);
-		exit;
-	}
 }
