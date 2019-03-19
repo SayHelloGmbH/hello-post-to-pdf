b18541ec8f34d46b607aa7476baacadfc9f188d1
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-03-19 15:33:02 +0100

    Codeing Standards

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -3,12 +3,12 @@ namespace SayHello\PostToPDF;
 
 use mikehaertl\wkhtmlto\Pdf;
 
-class Plugin
-{
+class Plugin {
+
 	private static $instance;
-	public $name = '';
+	public $name    = '';
 	public $version = '';
-	public $file = '';
+	public $file    = '';
 	private $allowed_post_types;
 
 	/**
@@ -18,17 +18,16 @@ class Plugin
 	 * @param  string $file The file from which the class is being instantiated.
 	 * @return object       The class instance.
 	 */
-	public static function getInstance($file = __FILE__)
-	{
-		if (!isset(self::$instance) && !(self::$instance instanceof Plugin)) {
+	public static function getInstance( $file = __FILE__ ) {
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Plugin ) ) {
 			self::$instance = new Plugin;
-			if (!function_exists('get_plugin_data')) {
+			if ( ! function_exists( 'get_plugin_data' ) ) {
 				require_once ABSPATH . 'wp-admin/includes/plugin.php';
 			}
-			$data = get_plugin_data($file);
-			self::$instance->name = $data['Name'];
+			$data                    = get_plugin_data( $file );
+			self::$instance->name    = $data['Name'];
 			self::$instance->version = $data['Version'];
-			self::$instance->file = $file;
+			self::$instance->file    = $file;
 			self::$instance->run();
 		}
 		return self::$instance;
@@ -38,39 +37,35 @@ class Plugin
 	 * Execution function which is to be called after the class has been initialized.
 	 * This contains hook and filter assignments, etc.
 	 */
-	private function run()
-	{
-		$this->allowed_post_types = ['post'];
+	private function run() {
+		$this->allowed_post_types = [ 'post' ];
 
-		add_action('plugins_loaded', [$this, 'loadPluginTextdomain']);
-		register_activation_hook(self::$instance->file, [$this, 'activate']);
-		add_action('init', [$this, 'rewrite']);
-		add_action('template_include', [$this, 'changeTemplate']);
+		add_action( 'plugins_loaded', [ $this, 'loadPluginTextdomain' ] );
+		register_activation_hook( self::$instance->file, [ $this, 'activate' ] );
+		add_action( 'init', [ $this, 'rewrite' ] );
+		add_action( 'template_include', [ $this, 'changeTemplate' ] );
 	}
 
-	public function activate()
-	{
-		set_transient('hello-post-to-pdf_flush', 1, 60);
+	public function activate() {
+		set_transient( 'hello-post-to-pdf_flush', 1, 60 );
 	}
 
 	/**
 	 * Load translation files from the indicated directory.
 	 */
-	public function loadPluginTextdomain()
-	{
-		load_plugin_textdomain('hello-post-to-pdf', false, dirname(plugin_basename($this->file)) . '/languages');
+	public function loadPluginTextdomain() {
+		load_plugin_textdomain( 'hello-post-to-pdf', false, dirname( plugin_basename( $this->file ) ) . '/languages' );
 	}
 
 	/**
 	 * Flush rewrite rules on plugin activation
 	 * @return void
 	 */
-	public function rewrite()
-	{
-		add_rewrite_endpoint('shpdf', EP_PERMALINK);
- 
-		if (get_transient('hello-post-to-pdf_flush')) {
-			delete_transient('hello-post-to-pdf_flush');
+	public function rewrite() {
+		 add_rewrite_endpoint( 'shpdf', EP_PERMALINK );
+
+		if ( get_transient( 'hello-post-to-pdf_flush' ) ) {
+			delete_transient( 'hello-post-to-pdf_flush' );
 			flush_rewrite_rules();
 		}
 	}
@@ -80,15 +75,14 @@ class Plugin
 	 * @param  string $template Default (current) template path
 	 * @return string           Potentially modified template path
 	 */
-	public function changeTemplate($template)
-	{
-		if (get_query_var('shpdf', false) !== false) {
-			$newTemplate = $this->getTemplate(true);
-			if ($newTemplate) {
+	public function changeTemplate( $template ) {
+		if ( get_query_var( 'shpdf', false ) !== false ) {
+			$newTemplate = $this->getTemplate( true );
+			if ( $newTemplate ) {
 				return $newTemplate;
 			}
 		}
- 
+
 		//Fall back to original template
 		return $template;
 	}
@@ -97,81 +91,95 @@ class Plugin
 	 * Find the template - is it in the Theme/Child Theme or in the Plugin?
 	 * @return mixed Path to the template file or null if none found
 	 */
-	private function getTemplate($url_request = false)
-	{
+	private function getTemplate( $url_request = false ) {
 		// Check theme / child theme directory
 		// Use the filter in your theme to customize the theme template array
-		
-		if (!in_array(get_post_type(), apply_filters('hello-post-to-pdf/allowed-post-types', $this->allowed_post_types))) {
-			wp_die('<p>'._x('PDF generation is not available for this content type.', 'Error message', 'hello-post-to-pdf').'</p>'.
+
+		if ( ! in_array( get_post_type(), apply_filters( 'hello-post-to-pdf/allowed-post-types', $this->allowed_post_types ) ) ) {
+			wp_die(
+				'<p>' . _x( 'PDF generation is not available for this content type.', 'Error message', 'hello-post-to-pdf' ) . '</p>' .
 				sprintf(
 					'<p><a href="%1$s">%2$s</a></p>',
 					get_permalink(),
-					_x('View original content', 'Link text on error page', 'hello-post-to-pdf')
-				), 403);
+					_x( 'View original content', 'Link text on error page', 'hello-post-to-pdf' )
+				),
+				403
+			);
 		}
 
-		$template = locate_template(apply_filters('hello-post-to-pdf/theme-templates', ['single-hello-post-to-pdf.php']));
-		if ($template == '') {
+		$template = locate_template( apply_filters( 'hello-post-to-pdf/theme-templates', [ 'single-hello-post-to-pdf.php' ] ) );
+		if ( $template == '' ) {
 			// Check plugin directory next
-			$template = plugin_dir_path(self::getInstance()->file) . 'templates/simple.php';
-			if (!file_exists($template)) {
+			$template = plugin_dir_path( self::getInstance()->file ) . 'templates/simple.php';
+			if ( ! file_exists( $template ) ) {
 				return null;
 			}
 		}
 
 		$filesystem = new FileSystem();
-		$filepath = $filesystem->getFilepath(get_the_ID());
+		$filepath   = $filesystem->getFilepath( get_the_ID() );
 
-		if ($filepath) {
+		if ( $filepath ) {
 			ob_start();
-			include($template);
+			include( $template );
 			$html = ob_get_contents();
 			ob_end_clean();
 
 			$pdf = $this->getPdfConvertorObject();
-			$pdf->addPage($html);
+			$pdf->addPage( $html );
 
-			if ($url_request) {
-				if (!$pdf->send()) {
-					if (defined('WP_DEBUG') && WP_DEBUG === true) {
+			if ( $url_request ) {
+				if ( ! $pdf->send() ) {
+					if ( defined( 'WP_DEBUG' ) && WP_DEBUG === true ) {
 						$error = $pdf->getError();
-						var_dump($error);
+						var_dump( $error );
 						exit;
 					}
 
-					wp_die('<p>'._x('An unavoidable error occurred when creating the PDF.', 'Error message', 'hello-post-to-pdf').'</p>'.
+					wp_die(
+						'<p>' . _x( 'An unavoidable error occurred when creating the PDF.', 'Error message', 'hello-post-to-pdf' ) . '</p>' .
 						sprintf(
 							'<p><a href="%1$s">%2$s</a></p>',
 							get_permalink(),
-							_x('View original content', 'Link text on error page', 'hello-post-to-pdf')
-						), 500);
+							_x( 'View original content', 'Link text on error page', 'hello-post-to-pdf' )
+						),
+						500
+					);
 
 					exit;
 				}
 			}
 
-			if (!$pdf->saveAs($filepath)) {
-				if (defined('WP_DEBUG') && WP_DEBUG === true) {
+			if ( ! $pdf->saveAs( $filepath ) ) {
+				if ( defined( 'WP_DEBUG' ) && WP_DEBUG === true ) {
 					$error = $pdf->getError();
-					var_dump($error);
+					var_dump( $error );
 					exit;
 				}
-				wp_die('<p>'._x('An unavoidable error occurred when saving the PDF to the server.', 'Error message', 'hello-post-to-pdf').'</p>'.
+				wp_die(
+					'<p>' . _x( 'An unavoidable error occurred when saving the PDF to the server.', 'Error message', 'hello-post-to-pdf' ) . '</p>' .
 						sprintf(
 							'<p><a href="%1$s">%2$s</a></p>',
 							get_permalink(),
-							_x('View original content', 'Link text on error page', 'hello-post-to-pdf')
-						), 500);
+							_x( 'View original content', 'Link text on error page', 'hello-post-to-pdf' )
+						),
+					500
+				);
 
 				exit;
 			}
 		}
 	}
 
-	public function getPdfConvertorObject()
-	{
-		if (defined('WP_ENV') && WP_ENV === 'VVV') {
+	public function getPdfConvertorObject() {
+
+		$language_slug = '';
+		// if ( function_exists( 'wpml_get_language_information' ) ) {
+		// 	$language_information = wpml_get_language_information( $post_id );
+		// 	$language_slug        = isset( $language_information['language_code'] ) && ! empty( $language_information['language_code'] ) ? '&lang=' . $language_information['language_code'] : '';
+		// }
+
+		if ( defined( 'WP_ENV' ) && WP_ENV === 'VVV' ) {
 			// This is the special version for a local VVV environment.
 			// You won't usually need to use this.
 			// Margins etc are set in mm
--- hello-post-to-pdf.php
+++ hello-post-to-pdf.php
@@ -10,34 +10,31 @@ Text Domain: hello-post-to-pdf
 Domain Path: /languages
 */
 
-if (version_compare(get_bloginfo('version'), '4.9', '<') || version_compare(PHP_VERSION, '7.1', '<')) {
-	function hello_post_to_pdf_compatibility_warning()
-	{
+if ( version_compare( get_bloginfo( 'version' ), '4.9', '<' ) || version_compare( PHP_VERSION, '7.1', '<' ) ) {
+	function hello_post_to_pdf_compatibility_warning() {
 		echo '<div class="error"><p>' . sprintf(
-			_x('“%1$s” requires PHP %2$s (or newer) and WordPress %3$s (or newer) to function properly. Your site is using PHP %4$s and WordPress %5$s. Please upgrade. The plugin has been automatically deactivated.', 'Plugin warning message', 'hello-post-to-pdf'),
+			_x( '“%1$s” requires PHP %2$s (or newer) and WordPress %3$s (or newer) to function properly. Your site is using PHP %4$s and WordPress %5$s. Please upgrade. The plugin has been automatically deactivated.', 'Plugin warning message', 'hello-post-to-pdf' ),
 			'PLUGIN NAME',
 			'7.1',
 			'4.9',
 			PHP_VERSION,
 			$GLOBALS['wp_version']
 		) . '</p></div>';
-		if (isset($_GET['activate'])) {
-			unset($_GET['activate']);
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
 		}
 	}
-	add_action('admin_notices', 'hello_post_to_pdf_compatibility_warning');
-	function hello_post_to_pdf_deactivate_self()
-	{
-		deactivate_plugins(plugin_basename(__FILE__));
+	add_action( 'admin_notices', 'hello_post_to_pdf_compatibility_warning' );
+	function hello_post_to_pdf_deactivate_self() {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
 	}
-	add_action('admin_init', 'hello_post_to_pdf_deactivate_self');
+	add_action( 'admin_init', 'hello_post_to_pdf_deactivate_self' );
 	return;
 } else {
 	require_once 'vendor/autoload.php';
 	require_once 'Classes/Plugin.php';
-	function hello_post_to_pdf_get_instance()
-	{
-		return SayHello\PostToPDF\Plugin::getInstance(__FILE__);
+	function hello_post_to_pdf_get_instance() {
+		 return SayHello\PostToPDF\Plugin::getInstance( __FILE__ );
 	}
 	hello_post_to_pdf_get_instance();
 }
